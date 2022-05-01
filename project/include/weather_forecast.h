3f3c89df537f24096c12391726baa0498fa6d0ff
Kevin Phillips <kphill55@jh.edu>
2022-05-01 01:41:25 -0400

    WIP almost finished Gaussian Variance calculation, build system wip

--- project/include/weather_forecast.h
+++ project/include/weather_forecast.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstdlib>
 #include <cstdint>
+#include <algorithm>
 
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
@@ -12,21 +13,40 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/cudaimgproc.hpp>
 
+#include<nlohman/json>
+
 #ifndef FORECAST_H
+
+// Feature to treat an image as a set of Gaussian 
+struct Forecast_Feature {
+    std::string weather;
+    // Means
+    double bmean;
+    double gmean;
+    double rmean;
+    // Variances
+    double bvar;
+    double gvar;
+    double rvar;
+};
 
 class JForecast {
     public:
         // Take a single picture using the jetson camera
         void take_picture();
-        // Take a collection of pictures
+        // Take a collection of pictures and write a json file containing all their features
         void train_model();
+        // Read a JSON file of training features and spit out an averaged representation of the training features
+        void generate_cache();
+        // Load the cached training features and compute the closest distance to a feature classify the image
         void forecast();
     private:
         void read_img(const std::string & image_file);
-        void thrust_gauss_mean_MLE(const cv::Mat & im);
-        void kernel_gauss_mean_MLE(const cv::Mat & im);
-        void thrust_gauss_var_MLE(const cv::Mat & im, double e);
-        void kernel_gauss_var_MLE(const cv::Mat & im, double e);
+        void thrust_gauss_mean_MLE(const cv::Mat & im, Forecast_Feature & ff);
+        void kernel_gauss_mean_MLE(const cv::Mat & im, Forecast_Feature & ff);
+        
+        void thrust_gauss_var_MLE(const cv::Mat & im, Forecast_Feature & ff);
+        void kernel_gauss_var_MLE(const cv::Mat & im, Forecast_Feature & ff);
         
         thrust::host_vector<unsigned char> b_host;
         thrust::host_vector<unsigned char> g_host;
--- project/src/weather_forecast.cc
+++ project/src/weather_forecast.cc
@@ -15,13 +15,56 @@ JForecast::read_image(const std::string & image_file) {
 }
 
 // The MLE of a Gaussian Mean is the sum of the samples / n
-JForecast::thrust_gauss_mean_MLE(const cv::Mat & img) {
+JForecast::thrust_gauss_mean_MLE(const cv::Mat & img, Forecast_Feature & ff) {
     // pixels (x,y,z) = (1,2,3) is (b,g,r) = (1,2,3).
     img.forEach<Pixel>([](const Pixel & pixel, const int position[]) -> void {
-        pixel.x = position[0];
-        pixel.y = position[1];
-        pixel.z = position[2];
+        // Copy pixel colors to host vectors
+        b_host[position[0]] = pixel.x;
+        g_host[position[1]] = pixel.y;
+        b_host[position[2]] = pixel.z;
     });
+
+    // Copy host vector to device vector
+    b_dev = b_host;
+    g_dev = g_host;
+    r_dev = r_host;
+
+    // Perform the accumulation
+    double b_sum = thrust::reduce(b_dev.begin(), b_dev.end());
+    double g_sum = thrust::reduce(g_dev.begin(), g_dev.end());
+    double r_sum = thrust::reduce(r_dev.begin(), r_dev.end());
+
+    // Divide by n to get the mean
+    ff.bmean = b_sum / img.size();
+    ff.gmean = g_sum / img.size();
+    ff.rmean = r_sum / img.size();
+}
+
+// The MLE of a Gaussian Variance is the sum of the samples / n
+JForecast::thrust_gauss_var_MLE(const cv::Mat & img, Forecast_Feature & ff) {
+    // pixels (x,y,z) = (1,2,3) is (b,g,r) = (1,2,3).
+    img.forEach<Pixel>([](const Pixel & pixel, const int position[]) -> void {
+        // Copy pixel colors to host vectors
+        b_host[position[0]] = pixel.x;
+        g_host[position[1]] = pixel.y;
+        b_host[position[2]] = pixel.z;
+    });
+
+    // Copy host vector to device vector
+    b_dev = b_host;
+    g_dev = g_host;
+    r_dev = r_host;
+
+    // Subtract the mean from each element
+    double b_sum = thrust::S(b_dev.begin(), b_dev.end(), );
+    double g_sum = thrust::reduce(g_dev.begin(), g_dev.end(), );
+    double r_sum = thrust::reduce(r_dev.begin(), r_dev.end(), );
+
+    // Square each element
+
+    // Reduce/accumulate the result
+
+    // Divide by n
 }
 
 JForecast::~JForecast() {
