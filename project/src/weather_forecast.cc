5e28d74da580e0d77739438f03480865455933f5
Kevin Phillips <kevin.phillips@jhuapl.edu>
2022-05-10 02:32:15 -0400

    WIP

--- project/src/weather_forecast.cc
+++ project/src/weather_forecast.cc
@@ -19,7 +19,7 @@ void JForecast::read_image(const std::string & image_file) {
 // The MLE of a Gaussian Mean is the sum of the samples / n
 void JForecast::populate_gmle_means(Forecast_Feature & ff, const cv::Mat & m) {
     // Upload the image to the GPU
-    static cv::cuda::GpuMat device_mat(m.rows, m.cols, CV_32S);
+    static cv::cuda::GpuMat device_mat(m.rows, m.cols);
     device_mat.upload(m);
     
     // Split into BGR channels
@@ -52,7 +52,7 @@ void JForecast::populate_gmle_means(Forecast_Feature & ff, const cv::Mat & m) {
 // The MLE of a Gaussian Variance is the sum of the (samples - mean)^2
 void JForecast::populate_gmle_vars(Forecast_Feature & ff, const cv::Mat & m) {
     // Upload the image to the GPU
-    static cv::cuda::GpuMat device_mat(m.rows, m.cols, CV_32S);
+    static cv::cuda::GpuMat device_mat(m.rows, m.cols);
     device_mat.upload(m);
 
     // Split into BGR channels
