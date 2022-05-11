012d62deb7fa62dff2d0b557d76421f6cfe5766c
Kevin Phillips <kevin.phillips@jhuapl.edu>
2022-05-11 07:00:20 -0400

    Trying this

--- project/tests/test_forecast.cc
+++ project/tests/test_forecast.cc
@@ -25,6 +25,19 @@
 
 //#include <nlohmann/json.hpp>
 
+struct Forecast_Feature {
+    // The weather classification
+    std::string weather;
+    // Means
+    double bmean;
+    double gmean;
+    double rmean;
+    // Variances
+    double bvar;
+    double gvar;
+    double rvar;
+};
+
 template<typename T>
 inline void print_vector(const std::vector<T> & vec) {
 	std::cout << "[";
@@ -48,37 +61,78 @@ inline void print_cvmat(const cv::Mat & img) {
 
 using uchar = unsigned char;
 
+// The MLE of a Gaussian Mean is the sum of the samples / n
+void populate_gmle_means(Forecast_Feature & ff, const cv::Mat & m) {
+    // Upload the image to the GPU
+    // cv::cuda::GpuMat device_mat(m.rows, m.cols, CV_32S);
+    cv::cuda::GpuMat device_mat;
+    device_mat.upload(m);
+    device_mat.convertTo(device_mat, CV_32S);
+    
+    // Split into BGR channels
+    std::vector<cv::cuda::GpuMat> channels(3);
+    cv::cuda::split(device_mat, channels);
+
+    cv::cuda::GpuMat b = channels[0];
+    cv::cuda::GpuMat g = channels[1];
+    cv::cuda::GpuMat r = channels[2];
+    
+    // Reduce the channels to their average, this returns one row (0 dimension)
+    cv::cuda::reduce(b, b, 0, cv::REDUCE_AVG);
+    cv::cuda::reduce(g, g, 0, cv::REDUCE_AVG);
+    cv::cuda::reduce(r, r, 0, cv::REDUCE_AVG);
+
+    cv::Mat b_result;
+    cv::Mat g_result;
+    cv::Mat r_result;
+
+    b.download(b_result);
+    g.download(g_result);
+    r.download(r_result);
+
+    // Store the average of each channel in the feature
+    ff.bmean = std::accumulate(b_result.begin<int>(), b_result.end<int>(), 0) / b_result.total();
+    ff.gmean = std::accumulate(g_result.begin<int>(), g_result.end<int>(), 0) / g_result.total();
+    ff.rmean = std::accumulate(r_result.begin<int>(), r_result.end<int>(), 0) / r_result.total();
+}
+
 int main(int argc, char * argv[]) {
     const cv::Mat img = cv::imread("jforecast_18324_s00_00000.jpg", cv::IMREAD_COLOR);
-    cv::cuda::GpuMat dst;
-    cv::cuda::GpuMat src;
-    src.upload(img);
+    // cv::cuda::GpuMat dst;
+    // cv::cuda::GpuMat src;
+    // src.upload(img);
 
-    src.convertTo(src, CV_32S);
-    // cv::cuda::subtract(src, 3, src);
-    // cv::cuda::sqr(src, src);
+    // src.convertTo(src, CV_32S);
+    // // cv::cuda::subtract(src, 3, src);
+    // // cv::cuda::sqr(src, src);
 
-    // Split the image into BGR
-    std::vector<cv::cuda::GpuMat> channels(3);
-    cv::cuda::split(src, channels);
+    // // Split the image into BGR
+    // std::vector<cv::cuda::GpuMat> channels(3);
+    // cv::cuda::split(src, channels);
 
-    // Take a single channel and get the average
-    cv::cuda::GpuMat b = channels[0];
-    cv::cuda::reduce(b, b, 0, cv::REDUCE_AVG);
+    // // Take a single channel and get the average
+    // cv::cuda::GpuMat b = channels[0];
+    // cv::cuda::reduce(b, b, 0, cv::REDUCE_AVG);
 
-    // Copy the result to the cpu
-    cv::Mat result;
-    b.download(result);
+    // // Copy the result to the cpu
+    // cv::Mat result;
+    // b.download(result);
 
-    // Accumulate the shortened array and find the average
-    std::cout << (int)result.at<char>(0,0) << "\n";
-    std::cout << result.size() << "\n";
-    std::vector<uchar> array(result.begin<int>(), result.end<int>());
+    // // Accumulate the shortened array and find the average
+    // std::cout << (int)result.at<char>(0,0) << "\n";
+    // std::cout << result.size() << "\n";
+    // std::vector<uchar> array(result.begin<int>(), result.end<int>());
 
-    double avg = std::accumulate(result.begin<int>(), result.end<int>(), 0) / result.total();
-    std::cout << avg << "\n";
+    // double avg = std::accumulate(result.begin<int>(), result.end<int>(), 0) / result.total();
+    // std::cout << avg << "\n";
     // print_vector(array);
 
+    Forecast_Feature ff;
+    Forecast_Feature feature{
+                "sunny",
+                -1,-1,-1,-1,-1,-1
+    };
+    populate_gmle_means(ff, img);
     //cv::imshow("Pic", result);
     //cv::waitKey(0);
     return 0;
